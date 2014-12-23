be1f82eec2f86df7e95c2328cca6039310f3bbbb
Tiago Floriano <tjfreelancer@mail.com>
2014-12-23 11:18:02 -0200

    Projeto adaptado para edição usando a ferramenta Intel XDK; implementação de função 'teste' para comunicação com o banco de dados.

--- inc/core.js
+++ inc/core.js
@@ -1,39 +1,27 @@
 // carrega variáveis
 $( document ).ready(function() {
-	$("title").html("Beleza Plena");
-	$("#projname").html("Beleza Plena");
+	$("title").html("Meu App");
+	$("#projname").html("Meu app");
 });
 
 // links
 $("#servicos").click(function() { abrePagina("servicos"); });
 
+// teste com função usando json para capturar, via api, dados armazenados na mongolab
 function listabancos() {
 	$("#div_listabancos").html("Carregando...");
-	/*$.getJSON(
-		"https://api.mongolab.com/api/1/databases/cadastros/collections/servicos?apiKey=vsdj4BRnrA9BBDoG1kqTnpB4dZlXsy8N", 
-		function(data){
-			$('#nome').text(data.nome);
-			$('#valor').text(data.valor);
-		}
-	);*/
-	/*$.getJSON( "https://api.mongolab.com/api/1/databases/cadastros/collections/servicos?apiKey=vsdj4BRnrA9BBDoG1kqTnpB4dZlXsy8N", function( data ) {
-		var items = [];
-		$.each( data, function( key, val ) {
-			items.push( "<li id='" + key + "'>" + val + "</li>" );
-		});
-		$( "<ul/>", {
-			"class": "my-new-list",
-			html: items.join( "" )
-		}).appendTo( "#div_listabancos" );
-	});*/
-	var dados = jQuery.parseJSON("https://api.mongolab.com/api/1/databases/cadastros/collections/servicos?apiKey=vsdj4BRnrA9BBDoG1kqTnpB4dZlXsy8N");
-	var i = 1;
-	var item = '';
-	while(i <= parseInt(dados.qtde_reg)){
-		item = item + "Nome: " + dados[i]['nome'] + ", Valor: " + dados[i]['valor'] + "<br>";
-		i++;
-	}
-	$('#div_listabancos').html(item);
+    $.getJSON( "https://api.mongolab.com/api/1/databases/cadastros/collections/servicos?apiKey=vsdj4BRnrA9BBDoG1kqTnpB4dZlXsy8N", function( data ) {
+        var items = [];
+        var contador = 0;
+        $.each( data, function( key, val ) {
+            items.push( "<li id='" + key + "'>" + val.nome + val.valor + key + "</li>" );
+        });
+        $("#div_listabancos").html("");
+        $( "<ul/>", {
+            "class": "my-new-list",
+            html: items.join( "" )
+        }).appendTo( "#div_listabancos" );
+	});
 }
 
 // funções diversas
